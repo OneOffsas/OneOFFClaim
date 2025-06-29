0ecb9a33b7459f0b24e23e40b2519d6f3c30e4ac
OneOffsas <benjamin-ruggieri@hotmail.fr>
2025-06-29 15:28:48 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,29 @@
 
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Register from "./pages/register";
 import Login from "./pages/login";
+import Register from "./pages/register";
 import Dashboard from "./pages/dashboard";
+import PrivateRoute from "./components/PrivateRoute";
 
-const App = () => {
+function App() {
   return (
     <Router>
-      <div className="bg-gray-100 min-h-screen">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<div className="p-10 text-xl">Bienvenue sur ClaimOneOff 🎯</div>} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route
+          path="/dashboard"
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        />
+        <Route path="*" element={<Login />} />
+      </Routes>
     </Router>
   );
-};
+}
 
 export default App;
