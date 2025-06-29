95679c5fda9d2685e3cb72f4f913e8fb77a5c932
OneOffsas <benjamin-ruggieri@hotmail.fr>
2025-06-29 16:22:12 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,17 @@
-// src/App.jsx
-
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./pages/login";
-import Register from "./pages/register";
-import Dashboard from "./pages/dashboard";
-import PrivateRoute from "./components/PrivateRoute";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/home';
+import Register from './pages/register';
+import Login from './pages/login';
+import Dashboard from './pages/dashboard';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
-        <Route
-          path="/dashboard"
-          element={
-            <PrivateRoute>
-              <Dashboard />
-            </PrivateRoute>
-          }
-        />
-        <Route path="*" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
   );
