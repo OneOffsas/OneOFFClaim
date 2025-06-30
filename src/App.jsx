74e5f6c89943f0703c970ac1aea0f3572fff7c1e
OneOffsas <benjamin-ruggieri@hotmail.fr>
2025-07-01 00:13:57 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,13 @@
-// src/App.jsx
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Landing from './pages/Landing';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Dashboard from './pages/Dashboard';
-import Navbar from './components/Navbar';
+// Tu ajouteras Register, Login, ForgotPassword plus tard
 
 function App() {
   return (
     <Router>
-      <Navbar />
       <Routes>
         <Route path="/" element={<Landing />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        {/* Ajout à venir : <Route path="/register" .../> */}
       </Routes>
     </Router>
   );
