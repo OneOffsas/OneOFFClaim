e5960ec29f55e508b8e727494b3431ed01cef292
OneOffsas <benjamin-ruggieri@hotmail.fr>
2025-07-02 00:36:40 +0200

    Create AdminDashboard.jsx

---
+++ src/pages/AdminDashboard.jsx
@@ -0,0 +1,13 @@
+// src/pages/AdminDashboard.jsx
+import React from 'react';
+
+function AdminDashboard() {
+  return (
+    <div style={{ padding: "2rem", textAlign: "center" }}>
+      <h1>Bienvenue dans l’espace admin 🎯</h1>
+      <p>Interface d'administration en construction.</p>
+    </div>
+  );
+}
+
+export default AdminDashboard;
