b7f54980a956f24c2c7f31fc49357c222c2c3955
OneOffsas <benjamin-ruggieri@hotmail.fr>
2025-07-02 00:37:20 +0200

    Create ClientDashboard.jsx

---
+++ src/pages/ClientDashboard.jsx
@@ -0,0 +1,13 @@
+// src/pages/ClientDashboard.jsx
+import React from 'react';
+
+function ClientDashboard() {
+  return (
+    <div style={{ padding: "2rem", textAlign: "center" }}>
+      <h1>Bienvenue dans l’espace client 📦</h1>
+      <p>Vous retrouverez ici toutes vos réclamations logistiques.</p>
+    </div>
+  );
+}
+
+export default ClientDashboard;
